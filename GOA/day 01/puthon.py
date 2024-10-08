f80107ac88cd16ce995a0e87bcc77af16b068b8c
dgebuadze1 <tsotnedgebuadze@gmail.com>
2024-10-08 23:02:42 +0400

    Add files via upload

---
+++ GOA/day 01/puthon.py
@@ -0,0 +1,10 @@
+name='me var cotne'
+
+surname='dgebuadze'
+
+age='16 wlis.'
+
+height=' simagleshi var 176 santimetri.'
+informetion='vswavlob meore namikolaos sajaro skolashi da var me-11 klasis moswavle.'
+
+print(name+' '+surname+' '+str(age)+' '+str(height)+' '+informetion)
